7a42238d6fa1f92c9118849178d601c7ed403a92
Isaac Warren <isaacwarren25@gmail.com>
2024-02-04 08:45:58 -0500

    Rational.java revised for BigInteger. TestRational.java created

--- src/Rational.java
+++ src/Rational.java
@@ -1,2 +1,125 @@
-package PACKAGE_NAME;public class Rational {
+import java.math.BigInteger;
+import java.util.Objects;
+
+public class Rational extends Number implements Comparable<Rational>{
+    // Data fields for numerator and denominator
+    private BigInteger[] r = new BigInteger[2];
+    private static final int NUMERATOR = 0;
+    private static final int DENOMINATOR = 1;
+
+    /** Construct a rational with default properties */
+    public Rational() {
+        this(BigInteger.ZERO, BigInteger.ONE);
+    }
+
+    /** Construct a rational with specified numerator and denominator */
+    public Rational(BigInteger numerator, BigInteger denominator) {
+        r[NUMERATOR] = numerator;
+        r[DENOMINATOR] = denominator;
+    }
+
+    /** Find GCD of two numbers */
+    private static BigInteger gcd(BigInteger n, BigInteger d) {
+
+        return n.gcd(d);
+    }
+
+    /** Return numerator */
+    public BigInteger getNumerator() {
+        return r[NUMERATOR];
+    }
+
+    /** Return denominator */
+    public BigInteger getDenominator() {
+        return r[DENOMINATOR];
+    }
+
+    /** Add a rational number to this rational */
+    public Rational add(Rational secondRational) {
+        BigInteger n = r[NUMERATOR].multiply(secondRational.getDenominator())
+                .add(r[DENOMINATOR].multiply(secondRational.getNumerator()));
+        BigInteger d = r[DENOMINATOR].multiply(secondRational.getDenominator());
+        return new Rational(n, d);
+    }
+
+    /** Subtract a rational number from this rational */
+    public Rational subtract(Rational secondRational) {
+        BigInteger n = r[NUMERATOR].multiply(secondRational.getDenominator())
+                .subtract(r[DENOMINATOR].multiply(secondRational.getNumerator()));
+        BigInteger d = r[DENOMINATOR].multiply(secondRational.getDenominator());
+        return new Rational(n, d);
+    }
+
+    /** Multiply a rational number to this rational */
+    public Rational multiply(Rational secondRational) {
+        BigInteger n = r[NUMERATOR].multiply(secondRational.getNumerator());
+        BigInteger d = r[DENOMINATOR].multiply(secondRational.getDenominator());
+        return new Rational(n, d);
+    }
+
+    /** Divide a rational number from this rational */
+    public Rational divide(Rational secondRational) {
+        BigInteger n = r[NUMERATOR].multiply(secondRational.getDenominator());
+        BigInteger d = r[DENOMINATOR].multiply(secondRational.getNumerator());
+        return new Rational(n, d);
+    }
+
+    /** Returns String representation of Rational number*/
+    @Override   // Override the toString method in the Object class
+    public String toString() {
+        if (Objects.equals(r[DENOMINATOR], BigInteger.ONE))
+            return r[DENOMINATOR] + "";
+        else
+            return r[NUMERATOR] + "/" + r[DENOMINATOR];
+    }
+
+    /** Returns whether two Rational numbers are equal*/
+    @Override   // Override the equals method in the Object class
+    public boolean equals(Object obj) {
+        if (!super.equals(obj))
+        {
+            return false;
+        }
+       // return (this minus (obj cast to Rational) equals zero
+        return (this.subtract((Rational) (obj))).getNumerator().equals(BigInteger.ZERO);
+    }
+
+    /** Return int value of Rational number*/
+    @Override   // Implement the abstract intValue method in Number
+    public int intValue() {
+        return (int)doubleValue();
+    }
+
+    /** Return float value of Rational number*/
+    @Override // Implement the abstract floatValue method in Number
+    public float floatValue() {
+        return (float)doubleValue();
+    }
+
+    /** Return double value of Rational number*/
+    @Override // Implement the doubleValue method in Number
+    public double doubleValue() {
+        double num1 = Double.parseDouble(r[NUMERATOR].toString());
+        double denom1 = Double.parseDouble(r[DENOMINATOR].toString());
+        return num1 / denom1;
+    }
+
+    /** Return long value of Rational number*/
+    @Override // Implement the abstract longValue method in Number
+    public long longValue() {
+        return (long)doubleValue();
+    }
+
+
+    @Override // Implement the compareTo method in Comparable
+    public int compareTo(Rational obj) {
+        if (this.subtract(obj).getNumerator().doubleValue() > 0){
+            return 1;
+        } else if (this.subtract(obj).getNumerator().doubleValue() < 0) {
+            return -1;
+        } else{
+            return 0;
+        }
+
+    }
 }
--- src/TestRational.java
+++ src/TestRational.java
@@ -1,2 +1,60 @@
-package PACKAGE_NAME;public class TestRational {
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * Program prompts user to enter two rational numbers.
+ * These numbers are added, subtracted, multiplied and divided.
+ * They are also compared using equals() and comparedTo()
+ * @author Isaac Warren
+ */
+
+public class TestRational {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); // Create Scanner object
+
+        //Prompt user to enter a rational number. Store numerator and Denominator
+        System.out.print("Enter the first rational number (num denom): ");
+        BigInteger numerator = input.nextBigInteger();
+        BigInteger denominator = input.nextBigInteger();
+
+        // Create first Rational object
+        Rational rational1 = new Rational(numerator, denominator);
+
+        //Prompt user to enter a rational number. Store numerator and Denominator
+        System.out.print("Enter the second rational number  (num denom): ");
+        numerator = input.nextBigInteger();
+        denominator = input.nextBigInteger();
+
+        // Create second Rational object
+        Rational rational2 = new Rational(numerator, denominator);
+
+        // Print output of operations
+        System.out.println(rational1 + " + " +
+                rational2 + " = " +
+                (rational1.add(rational2)));
+
+        System.out.println(rational1 + " - " +
+                rational2 + " = " +
+                (rational1.subtract(rational2)));
+
+        System.out.println(rational1 + " * " +
+                rational2 + " = " +
+                (rational1.multiply(rational2)));
+
+        System.out.println(rational1 + " / " +
+                rational2 + " = " +
+                (rational1.divide(rational2)));
+
+        System.out.println(rational2 + " is " +
+                rational2.doubleValue());
+
+        System.out.println(rational1 + " equals " +
+                rational2 + ": " +
+                rational1.equals(rational2));
+
+        System.out.println(rational1 + " compared to " +
+                rational2 + ": " +
+                rational1.compareTo(rational2));
+
+    }
 }
